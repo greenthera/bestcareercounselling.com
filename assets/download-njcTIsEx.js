d489ab9f0a753e6ba335ce66f83c3cedc95e5839
vama-shivantra <235020113+vama-shivantra@users.noreply.github.com>
2026-09-14 16:33:52 +0530

    Updates

--- assets/download-njcTIsEx.js
+++
@@ -1,1 +0,0 @@
-import{z as e}from"./index-Cdka3CW1.js";var t=e(`arrow-left`,[[`path`,{d:`m12 19-7-7 7-7`,key:`1l729n`}],[`path`,{d:`M19 12H5`,key:`x3x0zl`}]]),n=e(`download`,[[`path`,{d:`M12 15V3`,key:`m9g1x1`}],[`path`,{d:`M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4`,key:`ih7n3h`}],[`path`,{d:`m7 10 5 5 5-5`,key:`brsn70`}]]);export{t as n,n as t};
